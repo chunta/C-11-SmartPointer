9e1ca20cab6b4634db515e5ea8fbdaa8df421030
chunta <imchuntachen@gmail.com>
2019-04-18 12:03:15 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,46 @@
 #include <iostream>
 using namespace std;
 
+
+class String
+{
+    private:
+        unsigned int m_Size;
+        char * m_Buffer;
+    public:
+        String(const char *src)
+        {
+            m_Size = strlen(src);
+            m_Buffer = new char[m_Size+1];
+            memcpy(m_Buffer, src, m_Size);
+            m_Buffer[m_Size] = 0;
+        }
+
+        String(const String &src)
+        {
+            m_Size = src.m_Size;
+            m_Buffer = new char[m_Size+1];
+            memcpy(m_Buffer, src.m_Buffer, m_Size);
+            m_Buffer[m_Size] = 0;
+        }
+
+        char& operator[](unsigned int index)
+        {
+            return m_Buffer[index];
+        }
+
+        void echo()
+        {
+            printf("%s %lu %u\n", m_Buffer, strlen(m_Buffer), m_Size);
+        }
+
+        ~String()
+        {
+            delete[] m_Buffer;
+            printf("~String()\n");
+        }
+};
+
 class Entity
 {
 public:
@@ -50,5 +90,12 @@ int main()
             cout << "gw is expired\n";
         }
     }
+    
+    String a = "Hello";
+    String b = a;
+    b[2] = 'a';
+    a.echo();
+    b.echo();
+    
     return 0;
 }
